623ace45b6605fb56c7cc24260013aad1726b27b
Samuel Kogler <samuel.kogler@gmail.com>
2017-10-13 16:27:19 +0200

    Use new vagrant URL

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -5,7 +5,7 @@
 function add_vagrant_key {
     homedir=$(su - $1 -c 'echo $HOME')
     mkdir -p $homedir/.ssh
-    curl -L 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -o $homedir/.ssh/authorized_keys2
+    curl -L 'https://raw.github.com/hashicorp/vagrant/master/keys/vagrant.pub' -o $homedir/.ssh/authorized_keys2
     chown -Rf $1. $homedir/.ssh
     chmod 700 $homedir/.ssh
     chmod 600 $homedir/.ssh/authorized_keys2
